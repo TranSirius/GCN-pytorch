3ccc56eb16642a31fdc9fb8b379f35cadabf2d6e
TranSirius <transirius@outlook.com>
2019-08-20 19:08:42 +0800

    remote test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from utils.data_utils import *
-from utils.preprocess import *
-
-adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data('cora')
-
-r = sp_row_normalize(adj)
-print(r)
